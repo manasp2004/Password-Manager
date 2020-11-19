8f64bfc6c4ac55fb5bc57253de3706b1b90ad095
Manas Prakash <manasp2004@gmail.com>
2020-11-19 16:28:39 +0530

    It's Fucking Hard

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import getpass
 import base64
+import sqlite3
+
+conn = sqlite3.connect('passwords.db')
+
+c = conn.cursor()
+
+# c.execute("""CREATE TABLE passwords (
+#     org text,
+#     password text
+# )""")
 
 print("----------MENU----------")
 print("1. Add Password")
@@ -17,10 +27,19 @@ while choice < 1 or choice > 3 or choice == 99:
 def add_password():
     org = input("Enter the name of organization: ")
     password = getpass.getpass()
-    password.encode("ascii")
+    base64.b64encode(bytes(password, 'utf-8'))
+    c.execute("INSERT INTO passwords VALUES (?, ?)", (org, password))
 
 def show_password():
-    input("Enter the name of the Organization: ")
+    org = input("Enter the name of the Organization: ")
+    c.execute("SELECT * FROM passwords WHERE org=?", (org,))
+    my_pass = c.fetchone()
+
+    if my_pass == None:
+        print("This site has not been added!")
+
+    else:
+        print(my_pass)
 
 if choice == 1:
     add_password()
